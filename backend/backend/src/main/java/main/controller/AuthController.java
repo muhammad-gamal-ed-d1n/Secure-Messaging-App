a369a41c090ab8bfbfdc97474022ef74f9daee38
Zuhair Jarrar <es.zuhair.qussi2023@alexu.edu.eg>
2025-12-02 17:31:29 +0200

    login and signup

--- backend/backend/src/main/java/main/controller/AuthController.java
+++ backend/backend/src/main/java/main/controller/AuthController.java
@@ -4,16 +4,16 @@ import main.dto.JwtResponse;
 import main.dto.LoginRequest;
 import main.model.User;
 import main.security.JwtService;
+import main.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -23,6 +23,8 @@ public class AuthController {
 
     @Autowired
     private JwtService jwtService;
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest req) {
@@ -31,7 +33,8 @@ public class AuthController {
                 new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword())
         );
 
-        User user = (User) auth.getPrincipal();
+        UserDetails userd = (UserDetails) auth.getPrincipal();
+        User user = userService.findUserByUsername(userd.getUsername()).orElseThrow(() -> new RuntimeException("User not found"));
         String token = jwtService.generateToken(user);
 
         return ResponseEntity.ok(new JwtResponse(token));
--- backend/backend/src/main/java/main/security/JwtService.java
+++ backend/backend/src/main/java/main/security/JwtService.java
@@ -1,39 +1,61 @@
 package main.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import main.model.User;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
+
 @Service
 public class JwtService {
-    private final String SECRET = "5abbeeh fe application.yaml ba3deen";
-
+    private final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    private Key getSignKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
     public String generateToken(User user) {
         return Jwts.builder().
                 setSubject(user.getUsername()).
-                setIssuedAt(new Date()).
+                setIssuedAt(new Date(System.currentTimeMillis())).
                 setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)).
-                signWith(SignatureAlgorithm.HS256, SECRET).
+                signWith(getSignKey(),SignatureAlgorithm.HS256).
                 compact();
     }
 
     public String extractUsername(String token) {
-        return Jwts.parser().
-                setSigningKey(SECRET).
-                parseClaimsJwt(token).
-                getBody().
-                getSubject();
+        return extractClaim(token, Claims::getSubject);
     }
-
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder() // <--- FIX: parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token) // <--- FIX: parseClaimsJws (Signed)
+                .getBody();
+    }
     public boolean isValid(String token, User user) {
         try {
-            return extractUsername(token).equals(user.getUsername());
+            final String username = extractUsername(token);
+            return (username.equals(user.getUsername()) && !isTokenExpired(token));
         }
         catch (Exception e) {
             return false;
         }
+    }
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
