ea897c24f53dd0adf0bef9e129da271962023cf7
Zuhair Jarrar <es.zuhair.qussi2023@alexu.edu.eg>
2025-12-21 14:32:54 +0200

    correct position message and remove myself from new chat

--- frontend/src/app/add-chat-component/add-chat-component.ts
+++ frontend/src/app/add-chat-component/add-chat-component.ts
@@ -1,8 +1,18 @@
-import { ChangeDetectorRef, Component, EventEmitter, Output,AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Output,
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+  OnInit
+} from '@angular/core';
 import { User } from '../model/User';
 import { UserService } from '../service/user.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {AuthService} from '../auth/auth service/AuthService';
 
 @Component({
   selector: 'app-add-chat-component',
@@ -10,9 +20,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './add-chat-component.html',
   styleUrl: './add-chat-component.css',
 })
-export class AddChatComponent {
+export class AddChatComponent implements OnInit {
   @ViewChild('myInput') myInput!: ElementRef<HTMLInputElement>;
-
+  ngOnInit() {
+    this.authService.getCurrentUser().subscribe({
+      next: (curr: User) => {
+        this.currentUser = curr;
+        this.cdr.detectChanges();
+      }
+    })
+  }
   ngAfterViewInit(): void {
     this.myInput.nativeElement.focus();
   }
@@ -23,16 +40,17 @@ export class AddChatComponent {
 
   constructor(
     private userService: UserService,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private authService: AuthService
   ) { }
-
+  currentUser!: User;
   users: User[] = []
 
   searchForUser() {
     if (this.currentInput !== "") {
       this.userService.searchForUserByUsername(this.currentInput).subscribe({
         next: (res) => {
-          this.users = res;
+          this.users = res.filter(user => user.username !== this.currentUser.username);
           this.cdr.detectChanges()
         },
         error: (err) => {
